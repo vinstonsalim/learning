7734174776cc3c6ab51a522f57e13a6cec2483fb
Vinston Salim <vinston.salim@outlook.com>
2023-07-31 01:05:58 +0200

    Basic HTML with PHP

---
+++ PHP/php-simple-web/html.php
@@ -0,0 +1,16 @@
+<?php
+$titel = "Hi, mein Name ist Vinston";
+$beschreibung = "Ich bin PHP-Entwickler";
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title><?=$titel; ?></title>
+</head>
+<body>
+    <h1><?=$titel; ?></h1>
+    <p><?=$beschreibung; ?></p>
+</body>
+</html>
