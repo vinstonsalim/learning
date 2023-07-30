59105d86d925773209b5fe397025f576bf559cf9
Vinston Salim <vinston.salim@outlook.com>
2023-07-31 01:08:25 +0200

    Create simple view for testing routes

---
+++ PHP/php-simple-mvc/app/View/index.php
@@ -0,0 +1,6 @@
+<html lang="en">
+<body>
+<h1>Index php</h1>
+<p>My first paragraph.</p>
+</body>
+</html>
---
+++ PHP/php-simple-mvc/app/View/login.php
@@ -0,0 +1,6 @@
+<html lang="en">
+<body>
+<h1>Login php</h1>
+<p>My first paragraph.</p>
+</body>
+</html>
